10137cad8ac0aa3b60220dd344cdfa2b17e6c3ef
jose.boretto <jose.boretto@manomano.com>
2023-12-11 18:11:45 -0300

    in_memory_key_value_storage.go with *sync.Map

--- internal/infrastructure/persistance/in_memory_key_value_storage.go
+++ internal/infrastructure/persistance/in_memory_key_value_storage.go
@@ -1,41 +1,56 @@
 package persistance
 
-import "github.com/joseboretto/golang-crud-api/internal/domain/models"
+import (
+	"fmt"
+	"sync"
+
+	"github.com/joseboretto/golang-crud-api/internal/domain/models"
+)
 
 type InMemoryKeyValueStorage struct {
 	// This uses pointers over values because it can be nil
 	// This is not thread-safe, and it's not meant to be used in production.
 	// booksMap is a map of books, using the isbn as the key.
 	// In this case, the database is using the model, but it should be an Entity.
-	booksMap map[string]*models.Book
+	// map[string]*models.Book
+	booksMap *sync.Map
 }
 
 func NewInMemoryKeyValueStorage() *InMemoryKeyValueStorage {
 	return &InMemoryKeyValueStorage{
-		booksMap: make(map[string]*models.Book),
+		booksMap: &sync.Map{},
 	}
 }
 
 func (i *InMemoryKeyValueStorage) Insert(book *models.Book) (*models.Book, error) {
-	i.booksMap[book.Isbn] = book
+	i.booksMap.Store(book.Isbn, book)
 	return book, nil
 }
 
 func (i *InMemoryKeyValueStorage) GetAll() []*models.Book {
-	books := make([]*models.Book, 0, len(i.booksMap))
-	for _, value := range i.booksMap {
-		books = append(books, value)
-	}
+	books := make([]*models.Book, 0)
+	i.booksMap.Range(func(key, value interface{}) bool {
+		bookValue, ok := value.(*models.Book)
+		if !ok {
+			fmt.Println("Conversion failed")
+			return true
+		}
+		books = append(books, bookValue)
+		return true
+	})
+
 	return books
 }
 
 func (i *InMemoryKeyValueStorage) SelectBookByIsbn(isbn string) (*models.Book, bool) {
-	books, ok := i.booksMap[isbn]
-	return books, ok
+	value, ok := i.booksMap.Load(isbn)
+	bookValue, ok := value.(*models.Book)
+	return bookValue, ok
 }
 
 func (i *InMemoryKeyValueStorage) IncreaseBookViewsByIsbn(isbn string) {
-	books := i.booksMap[isbn]
-	books.Views++
-	i.booksMap[isbn] = books
+	value, _ := i.booksMap.Load(isbn)
+	bookValue, _ := value.(*models.Book)
+	bookValue.Views++
+	i.booksMap.Store(bookValue.Isbn, bookValue)
 }
